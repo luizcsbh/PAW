a3a5e955b4d4fda52f32e3b886ca0671008ce4eb
Luiz Santos <luizcsbh@gmail.com>
2019-05-16 17:24:49 -0300

    final

--- Projeto 1/scripts/main.js
+++ Projeto 1/scripts/main.js
@@ -1,8 +1,8 @@
-function selecionaImagemGaleria() {
-    document.getElementById("imagem-big").src = Event.target.src;
+function SelecionaImagemGaleria(event) {
+    document.getElementById("img-big").src = event.target.src;
 
     document.getElementById("img1").style.border = "none";
     document.getElementById("img2").style.border = "none";
     document.getElementById("img3").style.border = "none";
-    Event.target.style.border = "5px solid #81BEF7"
+    event.target.style.border = "5px solid #81BEF7";
 }
